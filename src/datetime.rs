0754f815a26f5bd4e5b9af85f3318267f7855e7f
Philipp Stucki <ps@stuckistucki.com>
2020-10-01 16:26:24 +0200

    removes some old stuff

--- src/datetime.rs
+++ src/datetime.rs
@@ -4,23 +4,6 @@ use std::fmt::Write;
 
 use chrono::{DateTime, Duration, FixedOffset, Local, TimeZone, Utc};
 
-// #[derive(Debug)]
-// pub struct DateTime<T: chrono::TimeZone> {
-//     datetime: chrono::DateTime<T>,
-// }
-
-// impl<T: chrono::TimeZone> From<&str> for DateTime<T> {
-//     fn from(as_string: &str) -> Self {
-//         DateTime {
-//             datetime: chrono::DateTime::from(
-//                 chrono::DateTime::parse_from_rfc3339(as_string).unwrap(),
-//             ),
-//         }
-//     }
-// }
-
-// impl From<&str> for chrono::DateTime
-
 pub fn from_rfc3339_string(as_string: &str) -> DateTime<FixedOffset> {
     DateTime::parse_from_rfc3339(as_string).unwrap()
 }
